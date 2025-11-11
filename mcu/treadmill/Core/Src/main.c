5724f4a6360f5dd3e9ed961e688797c401a68d87
Michael Panchenko <emb.wiser1201@gmail.com>
2025-11-11 18:59:26 +0200

    Init commit MCU code contains nothing, scheme and pcb has been already done.

---
+++ mcu/treadmill/Core/Src/main.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+void SystemClock_Config(void);
+static void GPIO_Init(void);
+
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+	GPIO_Init();
+
+	while (1)
+	{
+	}
+}
+
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	RCC_ClkInitStruct.ClockType =
+		RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+void GPIO_Init(void)
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio_led = {0};
+	gpio_led.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio_led.Pin = GPIO_PIN_13;
+	gpio_led.Pull = GPIO_PULLUP;
+	gpio_led.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOC, &gpio_led);
+}
+
+void Error_Handler(void)
+{
+	const uint32_t hclk_freq = HAL_RCC_GetSysClockFreq();
+	__disable_irq();
+	while (1)
+	{
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+		for (uint32_t i = 0; i < hclk_freq / 8; ++i)
+		{
+		}
+	}
+}
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
